bb6b5f335f3ebb61421451cd478af13220ed475c
Sankettarade <sankettarade2000@gmail.com>
2024-05-22 10:25:42 +0530

    commit the code

---
+++ src/e_commerce/OrderAction.java
@@ -0,0 +1,8 @@
+package e_commerce;
+
+public interface OrderAction {
+
+	   void placeOrder();
+	   void cancelOrder();
+	   void trackOrder();
+}
---
+++ src/e_commerce/Test.java
@@ -0,0 +1,62 @@
+package e_commerce;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+			
+		  // create products
+		Product product = new Product(1, "Shirt", 24.4, 2, "Coton");
+		Product product2 = new Product(2,"Mobile", 25000.00, 1, "Electronic");
+		product.getProductDetails();
+		product.updateQuantity(8);
+		product.updatePrice(34);
+			
+		  //create customer
+        Customer customer = new Customer(1, "abc", "abc@123", "abc@gmail.com");
+        customer.login();
+      //  customer.logout();
+        
+        // customer add products to cart
+        customer.addToCart(product);
+        customer.addToCart(product2);
+       
+        customer.removeFromCart(product);   //customer remove products from cart
+        
+        customer.viewProfile();  //customer view profile
+       
+        customer.updateProfile();   //customer update profile
+       
+        customer.checkout();    // Customer checks out
+        
+       // create admin 
+        Admin admin = new Admin(1, "admin", "admin@123", "admin@gmail.com");
+       System.out.println("Admin is working : ");
+       admin.addProduct(product);    // Admin add  the product
+     
+       admin.removeProduct(product2);  // Admin removes the product
+     
+       admin.updateProduct(product);   // Admin updates a product
+
+       admin.viewProfile();   // Admin views profile
+		
+       
+       // Create an order
+       List<Product> orderedProducts = new ArrayList<>();
+       
+       orderedProducts.add(product);
+       
+       Order order = new Order(1, customer.getUserId(), new Date(), "Processing", orderedProducts);
+   
+       order.placeOrder();     // Place the order 
+  
+       order.trackOrder();    // Track the order
+     
+       order.cancelOrder();  // Cancel the order
+       
+	}
+
+}
